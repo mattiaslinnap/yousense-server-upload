44b0ede5231b083e4d3108a94bd4b8a4d797027b
Mattias Linnap <mattias@linnap.com>
2013-01-02 22:00:47 +0200

    Views for debugging uploads

--- uploadapi/views.py
+++ uploadapi/views.py
@@ -1,15 +1,18 @@
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def file2(appid, installid):
+def file2(request, appid, installid):
     # Check client version.
     # Validate upload metadata: SHA1, client path.
     # Save data to DB.
-    pass
+    print request.body
+    return HttpResponse('{"success": true}')
 
 @csrf_exempt
-def status2(appid, installid):
+def status2(request, appid, installid):
     # Check client version.
     # Parse data, validate
     # Save status.
-    pass
+    print request.body
+    return HttpResponse('{"success": true}')
