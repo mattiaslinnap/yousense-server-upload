276a589798b0405b37d320ac26b46e8c2ef2128c
Mattias Linnap <mattias@linnap.com>
2013-05-14 23:38:05 +0100

    Changed INSTALLID_PATTERN to accept ids from 25 to 32 hex chars. 32 hex chars is expected, but for some reason 31 occur.

--- uploadapi/urls.py
+++ uploadapi/urls.py
@@ -4,7 +4,9 @@ from future_builtins import *  # ascii, filter, hex, map, oct, zip
 from django.conf.urls import patterns, url
 
 APPID_PATTERN = r'(?P<appid>[0-9a-zA-Z.]{1,100})'
-INSTALLID_PATTERN = r'(?P<installid>[0-9a-f]{32})'
+INSTALLID_PATTERN = r'(?P<installid>[0-9a-f]{25,32})'
+
+/upload/2/status/org.yousense.tartu/ccfc1a810aaf44aa738bcfdb65f4b32/
 
 urlpatterns = patterns('uploadapi.views',
 
